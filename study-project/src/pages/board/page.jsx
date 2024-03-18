ce2672375f54dd9e8c0e98926b65484ff0083498
Juunary <harry020104@naver.com>
2024-03-18 22:40:09 +0900

    Update page.jsx

--- study-project/src/pages/board/page.jsx
+++ study-project/src/pages/board/page.jsx
@@ -2,7 +2,9 @@ export default function BoardPage() {
 	return (
 		<div className="flex flex-col items-center justify-center ">
 			<div className="bg-backgroundGrey w-full">
-
+				<div className=" flex flex-col flex-grow mx-auto w-1/2">
+				<p>hi</p>
+				</div>
 			</div>
 		</div>
 	);
