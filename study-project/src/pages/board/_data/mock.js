724d5e7f4e4ba617625055b7f9d0377a337a3e0a
Juunary <harry020104@naver.com>
2024-04-02 19:22:25 +0900

    [ :sparkles: | HS2-16 | Feat ] 파일 구조 개선

---
+++ study-project/src/pages/board/_data/mock.js
@@ -0,0 +1,27 @@
+export const posts = [
+    {
+    title: '[React] 리액트에 대해 알아보자',
+    content: '리액트 어쩌구 저쩌구',
+    author: '김코딩',
+    },
+    {
+    title: '[JavaScript] 에 대해 알아보자',
+    content: '자바스크립트에서 어쩌구 저쩌구',
+    author: '이코드',
+    },
+    {
+    title: '[CSS] 반응형 웹 디자인에 대해 알아보자',
+    content: '디자인  어쩌구 저쩌구',
+    author: '박디자인',
+    },
+    {
+    title: '[Node.js] 서버 에 대해 알아보자',
+    content: 'Node.js 어쩌구 저쩌구',
+    author: '최백엔드',
+    },
+    {
+    title: '[Git] Git에 대해 알아보자',
+    content: 'Git 어쩌구 저쩌구',
+    author: '강개발자',
+    },
+];
